a116c9ec1e9adbfe0cc1dd07a39d9f90784c211e
raulgo <raul.ra.gonzalez@oracle.com>
2024-06-16 13:46:44 -0600

    whatsapp btn

--- js/main.js
+++ js/main.js
@@ -5,13 +5,28 @@
     $(document).ready(function () {
         function toggleNavbarMethod() {
             if ($(window).width() > 992) {
-                $('.navbar .dropdown').on('mouseover', function () {
+                $('.navbar .dropdown')
+                .on('mouseover', function () {
                     $('.dropdown-toggle', this).trigger('click');
-                }).on('mouseout', function () {
+                })
+                .on('mouseout', function () {
                     $('.dropdown-toggle', this).trigger('click').blur();
                 });
             } else {
                 $('.navbar .dropdown').off('mouseover').off('mouseout');
+            }
+
+            var eventhandler = function(e) {
+                e.preventDefault();      
+                //e.stopPropagation();
+             }
+
+            if ($(window).width() <= 768) {
+               // $("a.hover-button").bind('click', eventhandler);
+               //$("a.hover-button").prop('disabled', true);
+            }else {
+                //$("a.hover-button").prop('disabled', false);
+               // $("a.hover-button").unbind('click',eventhandler);
             }
         }
         toggleNavbarMethod();
