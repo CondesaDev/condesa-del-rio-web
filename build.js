13d191e0a296d6d70e6646abe36038dd03325c34
raulgo <raul.ra.gonzalez@oracle.com>
2024-06-29 21:33:32 -0600

    re write env over content

--- build.js
+++ build.js
@@ -1,11 +1,23 @@
 const fs = require('fs');
+const path = './js/main.js';
 
-// Crear el contenido de script.js con variables de entorno
-const scriptContent = `
+// Leer el contenido actual de script.js
+let scriptContent = '';
+if (fs.existsSync(path)) {
+  scriptContent = fs.readFileSync(path, 'utf8');
+}
+
+// Crear el contenido de las variables de entorno
+const envVariables = `
 const LOGIN_URL = "${process.env.LOGIN_URL}";
 
 console.log("LOGIN_URL:", LOGIN_URL);
+console.log("env:", process.env);
+// Rest of the script
 `;
 
-// Escribir el contenido en script.js
-fs.writeFileSync('./js/main.js', scriptContent);
+// Combinar las variables de entorno con el contenido existente
+const newContent = envVariables + scriptContent;
+
+// Escribir el contenido combinado en script.js
+fs.writeFileSync(path, newContent, 'utf8');
